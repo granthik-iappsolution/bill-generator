97dde106baef04ff9e38b13fcc8fa28a1f5d2fe6
granthik-iappsolution <granthik.iappsolution@gmail.com>
2024-11-28 12:43:14 +0530

    add(code): base project setup

---
+++ config/sidebar-menu.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    [
+        'name'      => 'Dashboard',
+        'icon'      => '<i class="menu-icon tf-icons bx bxs-dashboard"></i>',
+        'isHeader'  => false,
+        'route'     => 'dashboard',
+        'children'  => [],
+    ],
+    [
+        'name'      => 'Users',
+        'icon'      => '<i class="menu-icon tf-icons bx bx-user"></i>',
+        'isHeader'  => false,
+        'route'     => 'admin.users.index',
+        'children'  => [],
+    ],
+    [
+        'name'      => 'Roles',
+        'icon'      => '<i class="menu-icon tf-icons bx bx-lock-open-alt"></i>',
+        'isHeader'  => false,
+        'route'     => 'admin.roles.index',
+        'children'  => [],
+    ]
+];
